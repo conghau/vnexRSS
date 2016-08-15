5aa30bd1a0eb5709e79b44a1b6535b789bb524ec
conghau <conghau.master1@gmail.com>
2016-08-15 18:38:39 +0100

    update

---
+++ Mvc 5 Empty Template1/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using VNExpressFeed.Models;
+using VNExpressFeed.Utils;
+
+namespace VNExpressFeed.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+           
+            List<EndPointRSSModel> listRSSLink = VNExpressRssUtil.getEndPointRSS(Server.MapPath("~/ConfigRSS/vnexpress.rss.json"));
+            var a = VNExpressRssUtil.parserXML(listRSSLink[0].rss);
+            return View(a);
+        }
+
+       
+    }
+}
---
+++ Mvc 5 Empty Template1/Models/vnexpressRssModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VNExpressFeed.Models
+{
+    public class VNExpressRssModel
+    {
+        public String link;
+        public String description;
+        public String title;
+        public String image;
+    }
+
+    public class EndPointRSSModel
+    {
+        public String label;
+        public String rss;
+    }
+}
